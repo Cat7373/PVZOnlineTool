afc6eabedeaf7385d5a63d3a4e5c443ab386b31f
Cat73 <1901803382@qq.com>
2016-02-28 20:30:37 +0800

    修改程序名称

--- PVZOnlineTool/Properties/AssemblyInfo.cs
+++ PVZOnlineTool/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("植物大战僵尸Online脑力达人速查工具")]
+[assembly: AssemblyTitle("根据拼音首字母速查题库的小工具")]
 [assembly: AssemblyDescription("By: Cat73, QQ: 1901803382")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("植物大战僵尸Online脑力达人速查工具")]
+[assembly: AssemblyProduct("根据拼音首字母速查题库的小工具")]
 [assembly: AssemblyCopyright("Copyright © Cat73 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
