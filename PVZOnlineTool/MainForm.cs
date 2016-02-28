fa2e2f092270f3423d91cde07481a4a0203197da
Cat73 <1901803382@qq.com>
2016-02-28 14:02:31 +0800

    添加注释

--- PVZOnlineTool/MainForm.cs
+++ PVZOnlineTool/MainForm.cs
@@ -10,7 +10,13 @@ namespace PVZOnline {
         /// 题目表
         /// </summary>
         private Questions questions = new Questions();
+        /// <summary>
+        /// 默认标题
+        /// </summary>
         private String DEFAULT_TITLE;
+        /// <summary>
+        /// 当前输入的查询内容
+        /// </summary>
         private StringBuilder selectString = new StringBuilder();
 
         public MainForm() {
@@ -18,6 +24,7 @@ namespace PVZOnline {
         }
 
         private void MainForm_Load(object sender, EventArgs args) {
+            // 加载 info.txt
             FileStream infoFile = null;
             StreamReader streamReader = null;
             try {
@@ -35,6 +42,7 @@ namespace PVZOnline {
                 }
             }
             catch (Exception e) {
+                // 加载失败则设置标题为空
                 this.DEFAULT_TITLE = "";
             }
             finally {
@@ -50,20 +58,22 @@ namespace PVZOnline {
         }
 
         private void listBox1_KeyPress(object sender, KeyPressEventArgs e) {
+            // 处理输入字符
             char ch = e.KeyChar;
-            if (ch == '\r') {
+            if (ch == '\r') { // 回车 清除查询内容
                 this.selectString.Clear();
             }
-            else if (ch == '\b' && this.Text.Length >= 1) {
+            else if (ch == '\b' && this.Text.Length >= 1) { // 删除一个字符
                 this.selectString.Remove(this.selectString.Length - 1, 1);
             }
-            else if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z')) {
+            else if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z')) { // 0~9 a~z 将输入的字符添加到查询内容后面
                 this.selectString.Append(ch);
             }
             else {
                 return;
             }
 
+            // 如果查询内容有变化则刷新 listBox 的内容
             setTitle();
             listBox1.Items.Clear();
 
@@ -76,6 +86,9 @@ namespace PVZOnline {
             }
         }
 
+        /// <summary>
+        /// 自动设置窗口标题
+        /// </summary>
         private void setTitle() {
             if (this.selectString.Length > 0) {
                 this.Text = this.selectString.ToString();
